eab7d0e4458a3b49a04aa626798e8559dc701a52
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-31 16:58:14 +1100

    refactor modular structure

--- app/main.js
+++ app/main.js
@@ -19,25 +19,44 @@ const game = (function () {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     ]
+    const CONFIG = {
+        CELL_SIZE: 20,
+        CANVAS_HEIGHT: 400,
+        CANVAS_WIDTH: 600,
+        CELL_ALIVE_COLOUR: 'yellow',
+        CELL_DEAD_COLOUR: 'grey',
+        REFRESH_RATE: 500
+    }
 
-    const CELL_SIZE = 20;
-    const CANVAS_HEIGHT = 400;
-    const CANVAS_WIDTH = 600;
-    const CELL_ALIVE_COLOUR = 'yellow'
-    const CELL_DEAD_COLOUR = 'grey'
-
-    function init() {
+    const init = () => {
         const renderNextBtn = document.querySelector('.controls__next');
-        renderNextBtn.addEventListener('click', (e) => { updateGrid() })
-        updateGrid();
+        renderNextBtn.addEventListener('click', (e) => { control.next() })
+        const playLoop = document.querySelector('.controls__play');
+        playLoop.addEventListener('click', (e) => { control.loop() })
+        control.next();
     }
 
-    function updateGrid() {
-        grid = nextGrid();
-    }
+    const control = (() => {
+        let timer = null;
+        const loop = () => {
+            const { REFRESH_RATE } = CONFIG;
+            if (timer != null) {
+                clearInterval(timer)
+                timer = null;
+            } else {
+                timer = setInterval(control.next, REFRESH_RATE)
+            }
+        }
+        const next = () => { grid = nextGrid() }
+
+        return {
+            loop,
+            next
+        }
+    })()
 
 
-    function applyRulesToCell(y, x, cell) {
+    const applyRulesToCell = (y, x, cell) => {
         const alive = cell === 1 ? true : false;
         const neighboursCount = countNeighbours(y, x) // returns a number of neighbours
         let change = false;
@@ -45,91 +64,43 @@ const game = (function () {
         if (alive) {
             if (neighboursCount <= 1 || neighboursCount >= 4) {
                 change = 0 // dead :(
-            } 
-        } else if (!alive && neighboursCount === 3) { 
+            }
+        } else if (!alive && neighboursCount === 3) {
             change = 1; // comes back to life :)
         }
         return change === false ? cell : change;
 
     }
 
-    function countNeighbours(i, j) {
+    const countNeighbours = (i, j) => {
         let neighbours = 0;
+        const isRowAbove = i > 0;
+        const isRowBelow = i < grid.length - 1;
+        const isColLeft = j > 0;
+        const isColRight = j < grid[0].length;
 
-        const currentCellPos = { i, j }
-
-        const rowAbove = i > 0;
-        const rowBelow = i < grid.length - 1;
-        const colLeft = j > 0;
-        const colRight = j < grid[0].length;
-
-        // ROW ABOVE ------------------------------------
-
-        if (rowAbove) {
-            if (colLeft) {
-                // topLeft alive?
-                if (grid[i - 1][j - 1] === 1) {
-                    neighbours++
-                }
-            }
-            if (colRight) {
-                // topRight?
-                if (grid[i - 1][j + 1] === 1) {
-                    neighbours++
-                }
-            }
-            // top alive?
-            if (grid[i - 1][j] === 1) {
-                neighbours++
-            }
+        if (isRowAbove) {
+            grid[i - 1][j - 1] === 1 && isColLeft && neighbours++ // top left
+            grid[i - 1][j + 1] === 1 && isColRight && neighbours++ // top right
+            grid[i - 1][j] === 1 && neighbours++ // top
         }
 
-        // ROW BELOW ------------------------------------
-        if (rowBelow) {
-            if (colLeft) {
-                // bottomLeft alive?
-                if (grid[i + 1][j - 1] === 1) {
-                    neighbours++
-                }
-
-            }
-            if (colRight) {
-                // bottomRight alive?
-                if (grid[i + 1][j + 1] === 1) {
-                    neighbours++
-                }
-            }
-            // bottom alive?
-            if (grid[i + 1][j] === 1) {
-                neighbours++
-            }
-
+        if (isRowBelow) {
+            grid[i + 1][j - 1] && isColLeft && neighbours++// bottom left
+            grid[i + 1][j] === 1 && neighbours++ // bottom
+            grid[i + 1][j + 1] === 1 && isColRight && neighbours++ // bottom right
         }
 
-        // LEFT AND RIGHT ------------------------------------
-        if (colLeft) {
-            // left alive?
-            if (grid[i][j - 1] === 1) {
-                neighbours++
-            }
+        grid[i][j - 1] === 1 && neighbours++ // left
+        grid[i][j + 1] === 1 && neighbours++ // right
 
-        }
-        if (colRight) {
-            // right alive?
-            if (grid[i][j + 1] === 1) {
-                neighbours++
-            }
-
-        }
-        // return length of neighbour cells that are ALIVE
         return neighbours;
     }
 
-
-    const canvas = (function () {
+    const canvas = (() => {
         const canvas = document.querySelector('canvas')
         const ctx = canvas.getContext('2d');
-
+        const { CANVAS_HEIGHT, CANVAS_WIDTH, CELL_SIZE, CELL_ALIVE_COLOUR, CELL_DEAD_COLOUR } = CONFIG;
         const clear = () => {
             ctx.beginPath()
             ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -164,6 +135,7 @@ const game = (function () {
     init();
 
     return {
-        next: updateGrid
+        next: control.next,
+        loop: control.loop
     }
 })()
